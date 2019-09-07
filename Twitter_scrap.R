2cd3ffd8a7b7d5d0c84843e44a7c5155bc81fa9e
ferdeh <ferdeh.zulkarnain@gmail.com>
2019-09-07 15:35:04 +0000

    final

---
+++ Twitter_scrap.R
@@ -0,0 +1,37 @@
+# Mengambil Trend data Twitter
+# Import Library
+
+library(rtweet)
+library(compare)
+#token twitter
+
+token <- create_token(
+  consumer_key = "RYYCjCuXxQaVILbGJ1kGrPtUU",
+  consumer_secret = "rw7BkjhOrw8bl6qxKf6IUIMIeQ8Rq9MFvdt2HuWudaj2R3bewX",
+  access_token = "50967691-Z8rSTs6cfr5J6PCV2XXDicA2KNpAFbJQbUNgIOxZl",
+  access_secret = "KLEu0D1J0MnpcPpoUJeyVjP8GZjHV8wjc5mUnBHtBfbqz")
+
+#mengambil WOEID (Where on Earth ID Yahoo) dari available trend
+woeid<-trends_available(token = NULL, parse = TRUE)
+
+#Mencari WOEID Indonesia
+woeid_indo<-woeid[woeid$name == 'Indonesia',"woeid"]
+woeid_indo
+
+#Menampilkan trend yang ada di indonesia
+trends <- get_trends(woeid=woeid_indo[[1]])
+df_trends<-trends[c('trend','created_at')]
+df_trends
+
+# Membuat penyimpan trends
+#write.csv(df_trends,file = 'data_trend.csv')
+
+#Membaca data trend sebelumnya
+read_data<-read.csv(file = 'data_trend.csv',stringsAsFactors = FALSE)
+
+#menggabungkan data baru dengan data lama
+df_trend_union<- merge(df_trends,read_data[2:3],all = TRUE)
+head(df_trend_union)
+
+#menyimpan data gabungan ke file
+write.csv(df_trend_union,file = 'data_trend.csv')
